feaadb41ae6a39d4b5786b4127a53576feb03d0c
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-06-22 18:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function firstChar(text) {
-  // your code here
-	if(text[0]=' ') return text[1];
-	if(text.length==0) return '';
+  // your code here=
+	if(text.length>=1) return text.trim().charAt(0);
+	return text.charAt(0);
 }
 
 // Do not change the code below
+
 const text = prompt("Enter text:");
 alert(firstChar(text));
